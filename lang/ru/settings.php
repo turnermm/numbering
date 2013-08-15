8070a0d866e98a0bc80c2487c549e9800e8cfe9f
Myron Turner <turnermm02@shaw.ca>
2013-08-15 18:36:40 -0500

    Russian Language Update

---
+++ lang/ru/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Aleksandr Selivanov <alexgearbox@gmail.com>
+ */
+$lang['nstart']                = 'Число, с которого будет начинаться счёт';
