c9218f629fcdd6df681f00e20110b7c5f151bff3
Padhie <develop@padhie.de>
2015-08-22 11:10:34 +0200

    translation update

---
+++ lang/de/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Padhie <develop@padhie.de>
+ */
+$lang['nstart']                = 'Die Zahl bei welcher du anfangen möchtest zu zählen';
