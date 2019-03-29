c06268e2c3cb544f01f5913cb0472e3215b6d7ba
Myron Turner <turnermm02@shaw.ca>
2019-03-29 09:38:37 -0500

    Adds markup tag replaced in action.php with next number, adimed at automating numbering using namespace templates.

---
+++ action.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ *@author    Myron Turner <turnermm02@shaw.ca> 
+ DOKU_BASE + 'lib/plugins/numbering/scripts/getnum.php',
+ */
+ 
+if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../../').'/');
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+define ('NUMBERING_GETNUM', DOKU_PLUGIN . 'numbering/scripts/getnum.php');
+require_once(DOKU_PLUGIN.'action.php');
+class action_plugin_numbering extends DokuWiki_Action_Plugin {  
+   var $helper;
+        public function __construct()   {
+              $this->helper = plugin_load('helper', 'numbering');
+        }
+        public function register(Doku_Event_Handler $controller) {     
+          $controller->register_hook('COMMON_WIKIPAGE_SAVE', 'BEFORE', $this, 'handle_save',array('before'));   
+        }
+        
+        function handle_save(Doku_Event $event, $param) {   
+         if ($event->data['revertFrom']) return;
+          if(!$event->data['contentChanged'] ) return;
+          if(strpos($event->data['newContent'], '~~GetNextNumber~~') === false) return;
+    
+          $number = $this->getNextNumber();          
+          $event->data['newContent'] = str_replace('~~GetNextNumber~~', $number,$event->data['newContent']);
+        }
+        
+        function numberingDB() {
+        $db  = metaFN("numbering:seqnum",'.ser');
+        if(!file_exists($db)) {
+            io_saveFile($db,"", array());
+        }
+        return $db;
+      }
+      
+        function getNextNumber() {
+            $db = $this->numberingDB();
+            $start = $this->helper->getConfValue('nstart');            
+            io_lock($db);
+            $ar = unserialize(io_readFile($db,false));
+            if(!$ar) {   
+                $ar['saved'] = $start;
+                $ar['start'] = $start;        
+            }
+            else {       
+                $number = $ar['saved'];  
+                if($ar['start'] != $start) { 
+                     $ar['start'] = $start;
+                     $number = $start;
+                }
+                else $number = $ar['saved'];          
+            }
+            if($number < $start) $number = $start-1;
+            $ar['saved'] =  ++$number;
+            
+            file_put_contents($db,serialize($ar));
+            io_unlock($db);
+            return "$number";
+        }
+}  
